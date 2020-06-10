168a047fb1da66b93bfe4b46564084dd244affcd
Mgo <mgotm13@gmail.com>
2020-06-10 23:53:08 +0400

    Content Image loading

--- src/views/Home/Content.vue
+++ src/views/Home/Content.vue
@@ -1,5 +1,32 @@
 <template>
+    <div class="container">
+        <div class="home_container">
+            <md-card v-for="(post,index) in posts" :key="index">
+                <md-card-media md-ratio="4:3">
+                    <img :src="require(`../../assets/images/featured/${post.img}`)"/>
+                </md-card-media>
 
+                <md-card-header>
+                    <h2 class="md-title">{{ post.title }}</h2>
+                    <div class="md-subhead">
+                        <div>{{ post.description }}</div>
+                    </div>
+                </md-card-header>
+
+
+                <md-card-actions>
+                    <app-button
+                            type="link"
+                            :linkTo="`/posts/${post.id}`"
+                            :addClass="['small_link']"
+                    >
+                        See review
+                    </app-button>
+
+                </md-card-actions>
+            </md-card>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
@@ -9,7 +36,7 @@
 
     @Component
     export default class Content extends Vue {
-        posts:Array<IPosts> = posts;
+        private posts:Array<IPosts> = posts;
     }
 </script>
 
