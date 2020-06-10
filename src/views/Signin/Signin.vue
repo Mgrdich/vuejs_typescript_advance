be1a1b462f44c877691cf1609122e89ad929ea15
Mgo <mgotm13@gmail.com>
2020-06-11 00:00:41 +0400

    Form

---
+++ src/views/Signin/Signin.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <div class="signin_container">
+            <h1>Sign in</h1>
+            <form @submit.prevent="onSubmit" action="">
+
+            </form>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Vue, Component} from "vue-property-decorator";
+
+    @Component
+    export default class SignIn extends Vue {
+        public onSubmit():void {
+            
+        }
+
+    }
+</script>
