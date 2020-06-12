20da913eeb22f81f21311c89e0a5777355470e5b
Mgo <mgotm13@gmail.com>
2020-06-12 19:26:57 +0400

    creating the signin Page

--- src/views/Signin/Signin.vue
+++ src/views/Signin/Signin.vue
@@ -3,7 +3,15 @@
         <div class="signin_container">
             <h1>Sign in</h1>
             <form @submit.prevent="onSubmit" action="">
-
+                <div class="input_field">
+                    <label>Email</label>
+                    <input type="text" name="email" v-model="formData.email">
+                </div>
+                <div class="input_field">
+                    <label>Password</label>
+                    <input type="text" name="password" v-model="formData.password">
+                </div>
+                <button type="submit">Sign In</button>
             </form>
         </div>
     </div>
@@ -14,8 +22,12 @@
 
     @Component
     export default class SignIn extends Vue {
+        private formData:{email:string,password:string} = {
+            email:'',
+            password:''
+        }
         public onSubmit():void {
-            
+            console.log(this.formData);
         }
 
     }
